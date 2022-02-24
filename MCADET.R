fc11140763c3f618f04e556fc5aba670d03c8591
SaishiCui <95496138+SaishiCui@users.noreply.github.com>
2022-02-23 20:03:22 -0500

    Update MCADET.R

--- MCADET.R
+++ MCADET.R
@@ -1,59 +1,3 @@
-rm(list = ls())
-
-library(reticulate)
-py_available()
-py_module_available("numpy")
-py_config()
-
-
-library("Seurat")
-library("uwot")
-library("igraph")
-library("cccd")
-library("leiden")
-
-
-
-
-# Setup the Seurat Object
-
-
-# Load the PBMC dataset
-pbmc.data <- Read10X(data.dir = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/pbmc3k/filtered_gene_bc_matrices/hg19/")
-
-# Initialize the Seurat object with the raw (non-normalized data).
-pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
-
-
-
-
-## QC and selecting cells for further analysis ##
-
-# The [[ operator can add columns to object metadata. This is a great place to stash QC stats
-
-pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
-
-
-## We filter cells that have unique feature counts over 2,500 or less than 200 and 
-## We filter cells that have >5% mitochondrial counts
-
-
-
-pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
-
-raw_data=as.data.frame(pbmc[['RNA']]@data)
-
-
-
-
-
-mca.list.comp200<-mcadet(raw_data, n.comp = 200, run = 10, step = 2,seed = 1)
-mca.list.comp150<-mcadet(data=raw_data, n.comp = 150, run = 10, step = 2, seed = 1)
-mca.list.comp100<-mcadet(raw_data, n.comp = 100, run = 10, step = 2, seed = 1)
-mca.list.comp70<-mcadet(raw_data, n.comp = 70, run =10, step = 2, seed = 1)
-mca.list.comp50<-mcadet(raw_data, n.comp = 50, run = 10, step = 2, seed = 1)
-mca.list.comp30<-mcadet(raw_data, n.comp = 30, run = 10, step = 2, seed = 1)
-
 
 
 ### This function has 7 parameters in total:
@@ -64,13 +8,11 @@ mca.list.comp30<-mcadet(raw_data, n.comp = 30, run = 10, step = 2, seed = 1)
 
 ### 2). "n.comp" is the number of PCs selected from MCA decomposition, default is 100.
 
-### 3). "k" is number of K used to draw K-NN graph, default is 5.
+### 3). "k" is number of K used to draw K-NN graph, default is 0.05*(ncols of your input data set), namely, 0.05* the number of the cells.
 
-### 4). "run" is the number of iterations you want to run to get a more robust result, default is 1.
+### 4). "run" is the number of iterations you want to run to get a more robust result, default is 10.
 
-### 5). "step" should be specified when you want to do iterations, if your k=5, 
-###      run = 10, and step = 5, it means that you do 10 iterations on k={5,10,15,...50}, respectively.
-###      default is 0.
+### 5). "step" should be specified when you want to do iterations, default is 2.
 
 ### 6). "n.feature.1" is the number of genes to select in the first round, default is 3,000.
 
@@ -80,41 +22,6 @@ mca.list.comp30<-mcadet(raw_data, n.comp = 30, run = 10, step = 2, seed = 1)
 ### In order to run this function successfully, you need to install these packages: "irlba", "igraph", "cccd", 
 ### 'leidenalg', and "leiden"
 
-load(file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/sim.data.0.08.RData")
-load(file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/sim.data.0.05.RData")
-load(file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/sim.data.0.03.RData")
-load(file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/sim.data.0.3.RData")
-load(file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/sim.data.0.2.RData")
-load(file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/sim.data.0.1.RData")
-
-
-
-
-mca.sim.0.2<-mcadet(data=sim.data.0.2, n.comp=100, k=NA, run=10, step=2, seed=1, n.feature.1=3000, 
-                 n.feature.2=2000, clean=TRUE)
-mca.sim.0.1<-mcadet(data=sim.data.0.1, n.comp=100, k=NA, run=10, step=2, seed=1, n.feature.1=3000, 
-                    n.feature.2=2000, clean=TRUE)
-mca.sim.0.3<-mcadet(data=sim.data.0.3, n.comp=100, k=NA, run=10, step=2, seed=1, n.feature.1=3000, 
-                    n.feature.2=2000, clean=TRUE)
-
-
-
-mca.sim.0.08<-mcadet(data=sim.data.0.08, n.comp=100, k=NA, run=10, step=2, seed=1, n.feature.1=1500, 
-                    n.feature.2=1000, clean=TRUE)
-mca.sim.0.05<-mcadet(data=sim.data.0.05, n.comp=100, k=NA, run=10, step=2, seed=1, n.feature.1=1500, 
-                    n.feature.2=1000, clean=TRUE)
-mca.sim.0.03<-mcadet(data=sim.data.0.03, n.comp=100, k=NA, run=10, step=2, seed=1, n.feature.1=1500, 
-                    n.feature.2=1000, clean=TRUE)
-
-
-
-
-save(mca.sim.0.2,file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/mca.sim.0.2.RData")
-save(mca.sim.0.1,file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/mca.sim.0.1.RData")
-save(mca.sim.0.3,file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/mca.sim.0.3.RData")
-save(mca.sim.0.08,file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/mca.sim.0.08.RData")
-save(mca.sim.0.05,file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/mca.sim.0.05.RData")
-save(mca.sim.0.03,file = "C:/Users/cuisa/Desktop/BST_PhD/research/Dissertation/MCA/mca.sim.0.03.RData")
 
 
 
